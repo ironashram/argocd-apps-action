85062dbac401654a89849cf0bf401dd568e8347c
Michele Palazzi <sysdadmin@m1k.cloud>
2025-05-01 21:50:00 +0200

    test: add TestUpdateTargetRevision to validate target revision updates
    Signed-off-by: Michele Palazzi <sysdadmin@m1k.cloud>

--- src/argoaction/process_files_test.go
+++ src/argoaction/process_files_test.go
@@ -2,6 +2,8 @@ package argoaction
 
 import (
 	"net/http"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,6 +13,7 @@ import (
 	"github.com/ironashram/argocd-apps-action/internal"
 	"github.com/ironashram/argocd-apps-action/models"
 
+	"github.com/Masterminds/semver/v3"
 	"github.com/jarcoal/httpmock"
 )
 
@@ -106,4 +109,38 @@ spec:
 		mockOSInterface.AssertExpectations(t)
 		mockGitHubClient.AssertExpectations(t)
 	})
+}
+
+func TestUpdateTargetRevision(t *testing.T) {
+	mockAction := &internal.MockActionInterface{}
+	mockOSInterface := &internal.MockOS{}
+
+	fileContent := `spec:
+  source:
+    chart: chart1
+    repoURL: https://test.local
+    targetRevision: 0.1.2
+`
+	expectedContent := `spec:
+  source:
+    chart: chart1
+    repoURL: https://test.local
+    targetRevision: 0.2.0
+`
+	mockOSInterface.On("ReadFile", "test.yaml").Return([]byte(fileContent), nil)
+	mockOSInterface.On("WriteFile", "test.yaml", []byte(expectedContent), os.FileMode(0644)).Return(nil)
+
+	newest, _ := semver.NewVersion("0.2.0")
+
+	err := updateTargetRevision(newest, "test.yaml", mockAction, mockOSInterface)
+
+	assert.NoError(t, err)
+	mockOSInterface.AssertExpectations(t)
+
+	writeFileCall := mockOSInterface.Calls[1]
+	assert.Equal(t, "WriteFile", writeFileCall.Method)
+
+	writtenContent := string(writeFileCall.Arguments[1].([]byte))
+	assert.Equal(t, expectedContent, writtenContent)
+	assert.Equal(t, 5, strings.Count(writtenContent, "\n"))
 }
